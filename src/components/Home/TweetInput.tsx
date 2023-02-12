48a31a4fb82a991cc5af6265307d8721c85f4946
Eduardo Motta de Moraes <edumoraes@live.com>
2023-02-12 15:32:36 -0300

    refactor TweetInput to use state

--- src/components/Home/TweetInput.tsx
+++ src/components/Home/TweetInput.tsx
@@ -1,23 +1,25 @@
 import Avatar from "../generics/Avatar";
-import AvatarImg from "../../assets/avatar_example.jpeg";
 import Button from "../generics/Button";
 import { useRef, useState } from "react";
-import { flickerButtonRed } from "../../lib/uiUtils";
+import { type ProfileInfo } from "../../types";
+import { type DocWithNotFound } from "../../firebase/firestore";
 
-const TweetInput = () => {
-  const [showPlaceholder, setShowPlaceholder] = useState(true);
-  const [numOfCharacters, setNumOfCharacters] = useState(0);
+const TweetInput = ({
+  profileInfo,
+}: {
+  profileInfo: DocWithNotFound<ProfileInfo>;
+}) => {
+  const TWEET_LENGTH_LIMIT = 280;
+
+  const [inputText, setInputText] = useState("");
   const [loading, setLoading] = useState(false);
-  const [tweetButtonBgColor, setTweetButtonBgColor] = useState("bg-sky-500");
 
   const inputRef = useRef(null);
 
-  const tweetLengthLimit = 280;
-
   const clearInput = () => {
-    if (inputRef.current !== null && inputRef.current)
+    if (inputRef.current !== null)
       (inputRef.current as HTMLElement).innerText = "";
-    setShowPlaceholder(true);
+    setInputText("");
   };
 
   return (
@@ -26,20 +28,19 @@ const TweetInput = () => {
         <div className="absolute top-0 left-0 h-1 w-full bg-sky-500 animate-pulse" />
       )}
       <div className="flex gap-4">
-        <Avatar size="md" url={AvatarImg} />
+        <Avatar
+          size="md"
+          url={profileInfo?.avatar ?? ""}
+          handle={profileInfo?.handle ?? ""}
+        />
         <form
           id="tweet"
           action="#"
           className="flex flex-col w-full gap-4 pt-3 relative transition-all"
           onSubmit={(e) => {
             e.preventDefault();
-            const tweet = (e.target as HTMLElement).innerText;
-            setNumOfCharacters(tweet.length);
-
-            if (tweet.length === 0 || tweet.length > tweetLengthLimit) {
-              flickerButtonRed(setTweetButtonBgColor, "bg-sky-500");
-            } else if (tweet.length <= tweetLengthLimit) {
-              console.log(tweet);
+            if (inputText.length <= TWEET_LENGTH_LIMIT) {
+              console.log(inputText);
               setLoading(true);
               setTimeout(() => {
                 // todo: remove setTimeout
@@ -55,26 +56,30 @@ const TweetInput = () => {
             role="textbox"
             contentEditable
             onInput={(e) => {
-              setShowPlaceholder(e.currentTarget.innerText === "");
+              setInputText(e.currentTarget.innerText);
             }}
             ref={inputRef}
           />
         </form>
       </div>
-      {showPlaceholder && (
+      {inputText.length === 0 && (
         <div className="text-xl text-gray-500 absolute top-5 left-20 pointer-events-none">
           {"What's happening?"}
         </div>
       )}
       <div className="flex justify-end items-center gap-4 px-1">
-        {numOfCharacters < tweetLengthLimit + 1 || (
-          <span className="text-red-600">{280 - numOfCharacters}</span>
+        {inputText.length < TWEET_LENGTH_LIMIT + 1 || (
+          <span className="text-red-600">{280 - inputText.length}</span>
         )}
         <Button
           type="submit"
           form="tweet"
-          disabled={loading}
-          className={`text-white font-bold ${tweetButtonBgColor}`}
+          disabled={
+            loading ||
+            inputText.length === 0 ||
+            inputText.length > TWEET_LENGTH_LIMIT
+          }
+          className="text-white font-bold bg-sky-500"
         >
           Tweet
         </Button>
